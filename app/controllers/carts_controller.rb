1e62acec0b0d7a970f3ffa430cb1522a57447fcc
Greg Murrell <j_greg_m@comcast.net>
2022-02-04 15:39:51 -0500

    e3 written

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -49,14 +49,15 @@ class CartsController < ApplicationController
   end
 
   # DELETE /carts/1 or /carts/1.json
-  def destroy
-    @cart.destroy
-
-    respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+	def destroy
+		@cart.destroy if @cart.id == session[:cart_id]
+		session[:cart_id] = nil
+		respond_to do |format|
+			format.html { redirect_to store_index_url,
+			notice: 'Your cart is currently empty' }
+			format.json { head :no_content }
+		end
+	end
 
   private
     # Use callbacks to share common setup or constraints between actions.
