4ac4a3ff62b8aab1e9f352d5ed984956322791c9
jmurrel1 <jmurrel1@cs6242-vm01.uwg.westga.edu>
2022-02-06 14:39:54 -0500

    pay types jsx

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import NoPayType		from './NoPayType';
+import CreditCardPayType	from './CreditCardPayType';
+import CheckPayType		from './CheckPayType';
+import PurchaseOrderPayType	from './PurchaseOrderPayType';
+
 class PayTypeSelector extends React.Component {
+	constructor(props) {
+		super(props)
+		this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+		this.state = { selectedPayType: null };
+	}
+	onPayTypeSelected(event) {
+		this.setState({ selectedPayType: event.target.value });
+	}
 	render() {
+		let PayTypeCustomComponent = ??
 		return (
-			<div className="field">
-				<label htmlFor="order_pay_type">Pay type</label>
-				<select id="order_pay_type" name="order[pay_type]">
-					<option value="">Select a payment method</option>
-					<option value="Check">Check</option>
-					<option value="Credit card">Credit card</option>
-					<option value="Purchase order">Purchase order</option>
-				</select>
+			<div>
+				<div className="field">
+					<label htmlFor="order_pay_type">Pay type</label>
+					<select onChange={this.onPayTypeSelected} name="order[pay_type]">
+						<option value="">Select a payment method</option>
+						<option value="Check">Check</option>
+						<option value="Credit card">Credit card</option>
+						<option value="Purchase order">Purchase order</option>
+					</select>
+				</div>
+				<PayTypeCustomComponent/>
 			</div>
 		);
 	}
