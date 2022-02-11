bc33e440353657b37055cc996e528318a0b5c927
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2022-02-11 16:41:29 -0500

    set up for i18n react

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Depot2022
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.filter_parameters += [ :credit_card_number ]
+    config.middleware.use I18n::JS::Middleware
   end
 end
