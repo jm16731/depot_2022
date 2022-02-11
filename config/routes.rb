693f9e7d363b44b69a04927efbfdaf2eaf639c3e
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2022-02-11 16:02:30 -0500

    k1

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
   	delete 'logout' => :destroy
   end
 
-  #get 'sessions/create'
-  #get 'sessions/destroy'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
   resources :products do
   	get :who_bought, on: :member
   end
